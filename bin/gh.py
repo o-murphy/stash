ce3512ee56c0c05646309a6babff5bdd0ce66c6a
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2025-08-15 14:09:21 +0300

    gh.py update and fixes

--- bin/gh.py
+++ bin/gh.py
@@ -1,20 +1,10 @@
 # coding: utf-8
-"""
-Usage: gh <command> [<args>...]
-       gh <command> (-h|--help)
-supported commands are:
-        gh fork <repo>		forks user/repo
-        gh create <repo>		creates a new repo
-        gh pull <repo> <base> <head>	  create a pull request
-        gh list_keys	list user keys
-        gh create_key <title> [<public_key_path>] add a key to github (or create new key if none exist)
-For all commands, use gh <command> --help for more detailed help
-
-NOTE: assumes a keychain user/pass stored in 	keychainservice='stash.git.github.com', which is also the default from the git module.
-"""
-
+import argparse
 import os
-from functools import wraps
+
+
+import keychain
+import console
 
 _stash = globals()["_stash"]
 
@@ -23,81 +13,59 @@ try:
 except ImportError:
     print("Could not import 'github', installing it...")
     _stash("pip install pygithub")
-try:
-    import docopt
-except ImportError:
-    print("Could not import 'docopt', installing it...")
-    _stash("pip install docopt")
-from docopt import docopt
-from github import Github
-import keychain
-import console
 
 
 class GitHubRepoNotFoundError(Exception):
     pass
 
 
-def command(func):
-    @wraps(func)
-    def tmp(argv):
-        if len(argv) == 1:
-            if func.__name__ not in ["gh_list_keys"]:
-                argv.append("--help")
-        try:
-            args = docopt(func.__doc__, argv=argv)
-            return func(args)
-        except SystemExit as e:
-            print(e)
-
-    return tmp
-
-
-@command
-def gh_fork(args):
-    """Usage: gh fork <repo>
-
-    Fork a repo to your own github account.
-    <repo>	-  repo name of form user/repo
-
-    """
-    console.show_activity()
-    g, user = setup_gh()
+def setup_gh():
+    keychainservice = "stash.git.github.com"
     try:
-        other_repo = g.get_repo(args["<repo>"])
+        user = dict(keychain.get_services())[keychainservice]
+        pw = keychain.get_password(keychainservice, user)
+        g = github.Github(user, pw)
+        u = g.get_user()
+        return g, u
+    except KeyError:
+        print("Please set up your GitHub user/password in the keychain service.")
+        raise
+
+
+def gh_fork(args):
+    """Forks a repo to your own GitHub account."""
+    if console:
+        console.show_activity()
+    try:
+        g, user = setup_gh()
+        other_repo = g.get_repo(args.repo)
         if other_repo:
             mine = user.create_fork(other_repo)
             print("fork created: {}/{}".format(mine.owner.login, mine.name))
         else:
             pass
     finally:
-        console.hide_activity()
+        if console:
+            console.hide_activity()
 
 
-@command
 def gh_create(args):
-    """Usage: gh create [options] <name>
-
-    Options:
-    -h, --help             This message
-    -s <desc>, --description <desc>		Repo description
-    -h <url>, --homepage <url>				Homepage url
-    -p, --private          private
-    -i, --has_issues       has issues
-    -w, --has_wiki  			 has wiki
-    -d, --has_downloads    has downloads
-    -a, --auto_init     		create readme and first commit
-    -g <ign>, --gitignore_template <ign>  create gitignore using string
-
-
-    """
+    """Creates a new repository."""
     kwargs = {
-        key[2:]: value for key, value in args.items() if key.startswith("--") and value
+        "description": args.description,
+        "homepage": args.homepage,
+        "private": args.private,
+        "has_issues": args.has_issues,
+        "has_wiki": args.has_wiki,
+        "has_downloads": args.has_downloads,
+        "auto_init": args.auto_init,
+        "gitignore_template": args.gitignore_template,
     }
+    kwargs = {k: v for k, v in kwargs.items() if v is not None}
     console.show_activity()
     try:
         g, user = setup_gh()
-        r = user.create_repo(args["<name>"], **kwargs)
+        r = user.create_repo(args.name, **kwargs)
         print("Created %s" % r.html_url)
     finally:
         console.hide_activity()
@@ -116,52 +84,30 @@ def parent_owner(user, reponame):
     return user.get_repo(reponame).parent.owner.login
 
 
-@command
 def gh_pull(args):
-    """Usage:
-            gh pull <reponame> <base> [<head>]
-            gh pull <reponame> <base> [<head>] --title <title> [--body <body>]
-            gh pull <reponame> <base> [<head>] -i <issue>
-
-    Options:
-            -h, --help   							This message
-            -t <title>, --title <title>  	Title of pull request
-            -b <body>, --body <body>  		Body of pull request [default: ]
-            -i <issue>, --issue <issue>  	Issue number
-    Examples:
-            gh pull stash ywangd jsbain
-            gh pull stash ywangd:dev jsbain:dev
-            gh pull stash :dev :master
-
-            base and head should be in the format owner:branch.
-            if base owner is omitted, owner of parent repo is used.
-            if head owner is omitted, user is used
-    """
-
-    console.show_activity()
+    """Creates a pull request."""
+    if console:
+        console.show_activity()
     try:
         g, user = setup_gh()
-        reponame = args["<reponame>"]
-        baseowner, basebranch = parse_branch(args["<base>"])
+        reponame = args.reponame
+        baseowner, basebranch = parse_branch(args.base)
         if not baseowner:
-            baseowner = parent_owner(reponame)
-        if not args["<head>"]:
-            args["<head>"] = ":"
-        headowner, headbranch = parse_branch(args["<head>"])
+            baseowner = parent_owner(user, reponame)
+        if not args.head:
+            args.head = ":"
+        headowner, headbranch = parse_branch(args.head)
         if not headowner:
             headowner = user.login
 
         baserepo = g.get_user(baseowner).get_repo(reponame)
 
         kwargs = {}
-        if args["--issue"]:
-            kwargs["issue"] = baserepo.get_issue(args["--issue"])
-        elif not args["--title"]:
-            kwargs["title"] = input("Enter pull title:")
-            kwargs["body"] = input("Enter pull body:")
+        if args.issue:
+            kwargs["issue"] = baserepo.get_issue(args.issue)
         else:
-            kwargs["title"] = args["--title"]
-            kwargs["body"] = args["--body"] or ""
+            kwargs["title"] = args.title or input("Enter pull title:")
+            kwargs["body"] = args.body or input("Enter pull body:")
 
         kwargs["base"] = basebranch
         kwargs["head"] = ":".join([headowner, headbranch])
@@ -173,80 +119,91 @@ def gh_pull(args):
         print("Changed Files:")
         print([x.filename for x in pullreq.get_files()])
     finally:
-        console.hide_activity()
+        if console:
+            console.hide_activity()
     print("success")
 
 
-@command
 def gh_list_keys(args):
-    """Usage:
-                    gh list_keys [options]
-    Options:
-            -h, --help   							This message
-
-    List keys
-    """
+    """Lists user keys."""
     g, u = setup_gh()
     for key in u.get_keys():
         print("{}:\n {}\n".format(key.title, key.key))
 
 
-@command
 def gh_create_key(args):
-    """Usage:
-            gh create_key <title> [<public_key_path>]
-
-    Options:
-            -h, --help   							This message
-
-    Examples:
-            gh create_key ipad ~/.ssh/id_rsa.pub
-            gh create_key ipad    (checks for ~/.ssh/id_rsa.pub, or creates new key if needed using ssh-keygen )
-    """
-    title = args["<title>"]
+    """Adds a key to GitHub."""
+    title = args.title
     default_keyfile = os.path.expanduser("~/.ssh/id_rsa.pub")
-    if not args["<public_key_path>"]:
+    public_key_path = args.public_key_path
+    if not public_key_path:
         if not os.path.exists(default_keyfile):
             print("Creating a ssh key in ~/.ssh/")
-            cmd_string = """
-			echo ssh-keygen -d rsa -b2048
-			ssh-keygen -trsa -b2048
-			"""
-            _stash(cmd_string)
-        args["<public_key_path>"] = default_keyfile
+            _stash("ssh-keygen -t rsa -b 2048")
+        public_key_path = default_keyfile
+
     # if private key, use pub key
-    if not args["<public_key_path>"].endswith(".pub"):
-        args["<public_key_path>"] += ".pub"
-    if not os.path.exists(args["<public_key_path>"]):
+    if not public_key_path.endswith(".pub"):
+        public_key_path += ".pub"
+
+    if not os.path.exists(public_key_path):
         raise Exception("Public Key file not found!")
     g, u = setup_gh()
-    with open(args["<public_key_path>"]) as pubkey:
+    with open(public_key_path) as pubkey:
         u.create_key(title, pubkey.read())
 
 
-def setup_gh():
-    keychainservice = "stash.git.github.com"
-    user = dict(keychain.get_services())[keychainservice]
-    pw = keychain.get_password(keychainservice, user)
-    g = Github(user, pw)
-    u = g.get_user()
-
-    return g, u
-
-
 if __name__ == "__main__":
-    import sys
+    # Main parser setup
+    parser = argparse.ArgumentParser(
+        prog="gh",
+        description="A command-line tool for GitHub operations.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
 
-    if len(sys.argv) == 1:
-        sys.argv.append("--help")
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
 
-    args = docopt(__doc__, version="0.1", options_first=True)
-    cmd = args["<command>"]
-    argv = [cmd] + args["<args>"]
-    try:
-        func = locals()["gh_%s" % cmd]
-    except KeyError:
-        print("No such cmd")
-        print(__doc__)
-        raise
-    func(argv)
+    # Add subparsers for each command
+    subparsers = parser.add_subparsers(dest="command", required=True, help="supported commands")
+
+    # gh fork <repo>
+    fork_parser = subparsers.add_parser("fork", help="forks a user/repo")
+    fork_parser.add_argument("repo", help="repo name of form user/repo")
+    fork_parser.set_defaults(func=gh_fork)
+
+    # gh create <repo>
+    create_parser = subparsers.add_parser("create", help="creates a new repo")
+    create_parser.add_argument("name", help="repo name")
+    create_parser.add_argument("-s", "--description", help="Repo description")
+    create_parser.add_argument("-H", "--homepage", help="Homepage url")
+    create_parser.add_argument("-p", "--private", action="store_true", help="private repo")
+    create_parser.add_argument("-i", "--has_issues", action="store_true", help="has issues")
+    create_parser.add_argument("-w", "--has_wiki", action="store_true", help="has wiki")
+    create_parser.add_argument("-d", "--has_downloads", action="store_true", help="has downloads")
+    create_parser.add_argument("-a", "--auto_init", action="store_true", help="create readme and first commit")
+    create_parser.add_argument("-g", "--gitignore_template", help="create gitignore using string")
+    create_parser.set_defaults(func=gh_create)
+
+    # gh pull <repo> <base> <head>
+    pull_parser = subparsers.add_parser("pull", help="create a pull request")
+    pull_parser.add_argument("reponame", help="repo name")
+    pull_parser.add_argument("base", help="base owner:branch")
+    pull_parser.add_argument("head", nargs="?", help="head owner:branch [default: :]")
+    pull_parser.add_argument("--title", "-t", help="Title of pull request")
+    pull_parser.add_argument("--body", "-b", help="Body of pull request")
+    pull_parser.add_argument("--issue", "-i", help="Issue number")
+    pull_parser.set_defaults(func=gh_pull)
+
+    # gh list_keys
+    list_keys_parser = subparsers.add_parser("list_keys", help="list user keys")
+    list_keys_parser.set_defaults(func=gh_list_keys)
+
+    # gh create_key <title> [<public_key_path>]
+    create_key_parser = subparsers.add_parser("create_key", help="add a key to github")
+    create_key_parser.add_argument("title", help="key title")
+    create_key_parser.add_argument("public_key_path", nargs="?", help="path to public key file")
+    create_key_parser.set_defaults(func=gh_create_key)
+
+    # Parse args and call the function
+    args = parser.parse_args()
+    args.func(args)
